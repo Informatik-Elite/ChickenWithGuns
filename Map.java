bc54b1301b1afc5970350d2d262a798d06adfbc7
Robin <o.treptow@ggv.schule>
2025-02-04 09:00:16 +0100

    asd

--- Map.java
+++ Map.java
@@ -19,12 +19,6 @@ public class Map
         x = 0;
     }
 
-    /**
-     * Ein Beispiel einer Methode - ersetzen Sie diesen Kommentar mit Ihrem eigenen
-     * 
-     * @param  y    ein Beispielparameter für eine Methode
-     * @return        die Summe aus x und y
-     */
     public int beispielMethode(int y)
     {
         // tragen Sie hier den Code ein
