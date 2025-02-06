b1a7c68003e51b19c37dfdac58ea76dd7f4c395a
Informatik-Elite <o.treptow@ggv.schule>
2025-02-06 09:02:50 +0100

    kk

--- Revolver.java
+++ Revolver.java
@@ -1,36 +1,3 @@
-<<<<<<< HEAD
-
-/**
- * Beschreiben Sie hier die Klasse Revolver.
- * 
- * @author (Ihr Name) 
- * @version (eine Versionsnummer oder ein Datum)
- */
-public class Revolver
-{
-    // Instanzvariablen - ersetzen Sie das folgende Beispiel mit Ihren Variablen
-    private int x;
-
-    /**
-     * Konstruktor für Objekte der Klasse Revolver
-     */
-    public Revolver()
-    {
-        // Instanzvariable initialisieren
-        x = 0;
-    }
-
-    /**
-     * Ein Beispiel einer Methode - ersetzen Sie diesen Kommentar mit Ihrem eigenen
-     * 
-     * @param  y    ein Beispielparameter für eine Methode
-     * @return        die Summe aus x und y
-     */
-    public int beispielMethode(int y)
-    {
-        // tragen Sie hier den Code ein
-        return x + y;
-=======
 import SGE.*;
 public class Revolver extends Weapon
 {
@@ -47,6 +14,6 @@ public class Revolver extends Weapon
     public void reload()
     {
     
->>>>>>> 053a9afd549135779bba7bf50135af7e771e1376
+
     }
 }
